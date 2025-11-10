e5d4e491b1969d86b4295fcf902696976ad571bf
Sanjyrana <Sanjayrana5113@gmail.com>
2025-11-10 17:37:36 +0530

    routes setup + components

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+
+const Home = () => {
+  return (
+    <>
+     <Navbar/> 
+     <Outlet/>
+     <Footer/>
+    </>
+  )
+}
+
+export default Home
