e8775d4d1b78c8ad6e9de8d8183d8059b39a9a11
Sanjyrana <Sanjayrana5113@gmail.com>
2025-11-17 17:17:39 +0530

    feedslice

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios';
+import { addFeed } from '../Store/feedSlice';
+import toast from 'react-hot-toast';
+
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/feed`, { withCredentials: true })
+      dispatch(addFeed(res?.data?.data));
+      console.log(res.data.data)
+    } catch (error) {
+      toast.error("Feed fetch error:",error);
+    }
+  }
+
+  useEffect(() => {
+    getFeed();
+  }, [])
   return (
     <div>
       feed
