08aacec55a8f6b40b1fc1425d9f062a9aa6c963e
Sanjyrana <Sanjayrana5113@gmail.com>
2025-11-17 15:46:54 +0530

    logout feature

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,41 +1,83 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { removeUser } from '../Store/userSlice';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 
 function Navbar() {
-  const user = useSelector((store) => store.user);  
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+  const handleLogOut = async () => {
+    try {
+      const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/logout`, {}, {
+        withCredentials: true
+      });
+      dispatch(removeUser());
+      setDropdownOpen(false);
+      toast.success(res.data);
+      navigate("/login");
+    } catch (error) {
+      toast.error(error?.response?.data || "Something went wrong");
+    }
+  };
+
+  // Close dropdown if clicked outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
   return (
     <div className="navbar bg-gray-300 shadow-sm">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
       </div>
       <div className="flex gap-2">
-        <div className="dropdown dropdown-end mx-5">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-            { 
-              user && <div className="w-10 rounded-full">
+        <div ref={dropdownRef} className="relative">
+          <div
+            className="btn btn-ghost btn-circle avatar"
+            onClick={toggleDropdown}
+          >
+            {user && (
+              <div className="w-10 rounded-full">
                 <img
                   alt="user photo"
-                  src={user?.photoUrl || "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"} 
-                  />
+                  src={user?.photoUrl || "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"}
+                />
               </div>
-            }
+            )}
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li>
-              <Link to="/profile" className="justify-between">
-                Profile
-              </Link>
-            </li>
-            <li><a>Settings</a></li>
-            <li><a>Logout</a></li>
-          </ul>
+
+          {dropdownOpen && (
+            <ul className="absolute right-0 mt-2 w-52 p-2 bg-base-100 rounded-box shadow menu menu-sm z-50">
+              <li>
+                <Link to="/profile" onClick={() => setDropdownOpen(false)}>Profile</Link>
+              </li>
+              <li>
+                <Link onClick={() => setDropdownOpen(false)}>Settings</Link>
+              </li>
+              <li>
+                <Link to="/" onClick={handleLogOut}>Logout</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
